e16e84cf4bb0c90ce232d8b18e7e29689d111a4b
monikasananse <monikasnns@gmail.com>
2022-08-07 23:20:23 +0530

    done

--- file/src/App.js
+++ file/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Counter from './components/Counter';
 import ForUnderstanding from './components/ForUnderstanding';
 
 function App() {
+  return(
+    <>
   <ForUnderstanding/>
+  <Counter/>
+  </>
+  )
+
 }
 
 export default App;
---
+++ file/src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState,useEffect } from "react";
+
+const Counter=()=>{
+    const [count,setCount]=useState(10)
+
+    useEffect(()=>{
+        const id=setInterval(()=>{
+            setCount(function(prev){
+                if(prev===0)
+                {
+                    clearInterval(id);
+                    return prev
+                }
+                return prev-1;
+            });
+            },1000);
+    },[])
+     return(
+        <>
+        <h1>counter {count}</h1>
+        </>
+
+    )
+}
+export default Counter;
